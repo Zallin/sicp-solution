8d417c0dc41e1e89db4a5c58349351bb004f5f25
Tim Zallin <tim.zallin@gmail.com>
2015-09-15 21:24:05 +0300

    1.38 solution and tests added

---
+++ chapter1/1.38.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require "./1.37.rkt")
+
+(define (e-approx n)
+
+  (define (d i)
+
+    (define (find-k i k)
+      (cond ((< i 0) -1)
+            ((= i 2) k)
+            (else (find-k (- i 3) (+ k 1)))))
+
+    (let ((k (find-k i 0)))
+      (cond ((< k 0) 1)
+            (else (+ (* k 2) 2)))))
+
+  (+ (cont-frac-iter (lambda (i) 1.0) d n) 2.0))
+
+(provide e-approx)
