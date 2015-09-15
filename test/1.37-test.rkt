7b2e701bd392e09fe360b08d95ff2ec3e73c3c7b
Tim Zallin <tim.zallin@gmail.com>
2015-09-15 19:52:11 +0300

    1.37 solution and tests added

---
+++ test/1.37-test.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require rackunit
+         "../chapter1/1.37.rkt")
+
+(check-equal? (cont-frac-rec (lambda (i) 2.0)
+                         (lambda (i) 2.0)
+                         3)  0.75 "continued fraction with ni=2, di=2 failed")
+
+(check-equal? (cont-frac-rec (lambda (i) 1.0)
+                             (lambda (i) 1.0)
+                             5) 0.625 "continued fraction with ni=1, di=2 failed")
+
+(check-equal? (cont-frac-iter (lambda (i) 2.0)
+                         (lambda (i) 2.0)
+                         3)  0.75 "continued fraction with ni=2, di=2 failed")
+
+(check-equal? (cont-frac-rec (lambda (i) 1.0)
+                             (lambda (i) 1.0)
+                             5) 0.625 "continued fraction with ni=1, di=2 failed")
