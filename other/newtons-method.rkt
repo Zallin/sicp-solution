be2042ecc2e9325459ca3e015339aac56f1eec97
Tim Zallin <tim.zallin@gmail.com>
2015-09-19 21:34:21 +0300

    modified

--- other/newtons-method.rkt
+++ other/newtons-method.rkt
@@ -1,20 +1,8 @@
 #lang racket
 
-(define tolerance 0.001)
+(require "./fixed-point.rkt")
+
 (define dx 0.00001)
-
-(define (fixed-point f first-guess)
-
-  (define (good-enough? v1 v2)
-    (< (abs (- v1 v2)) tolerance))
-
-  (define (try guess)
-    (let ((next (f guess)))
-      (if (good-enough? guess next)
-          next
-          (try next))))
-
-  (try first-guess))
 
 (define (newtons-method g guess)
 
