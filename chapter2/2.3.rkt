00a61d3ee6099c333a065c7a9106f0f723939386
Tim Zallin <tim.zallin@gmail.com>
2015-09-24 20:10:50 +0300

    2.3 solution and tests added

---
+++ chapter2/2.3.rkt
@@ -0,0 +1,54 @@
+#lang racket
+
+(require "./2.2.rkt")
+
+; auxiliary square proc
+
+(define (square x) (* x x))
+
+; procedure that counts length of a segment
+
+(define (segment-length s)
+  (sqrt (+ (square (- (x-point (end-segment s)) (x-point (start-segment s))))
+           (square (- (y-point (end-segment s)) (y-point (start-segment s)))))))
+
+; first absraction that implements rectangle as two adjacent segments from 2.2
+
+(define (make-rect-first segment-a segment-b)
+  (cons segment-a segment-b))
+
+(define (rect-width-first r)
+  (segment-length (car r)))
+
+(define (rect-height-first r)
+  (segment-length (cdr r)))
+
+; second absraction that implements rectangle as points at the ends of rect diagonal
+
+(define (make-rect-second point-a point-b)
+  (cons point-a point-b))
+
+(define (rect-width-second r)
+  (- (x-point (cdr r)) (x-point (car r))))
+
+(define (rect-height-second r)
+  (- (y-point (car r)) (y-point (cdr r))))
+
+; square and perim ops modified so that procs that find width and height of a rectangle are given as arguments
+; this is done solely for the sake convenience (no need to double code below)
+
+(define (square-rect r rect-width rect-height)
+  (* (rect-width r) (rect-height r)))
+
+(define (perim-rect r rect-width rect-height)
+  (* 2 (+ (rect-width r) (rect-height r))))
+
+(provide segment-length
+         make-rect-first
+         rect-width-first
+         rect-height-first
+         make-rect-second
+         rect-width-second
+         rect-height-second
+         square-rect
+         perim-rect)
---
+++ test/2.3-test.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(require rackunit
+         "../chapter2/2.3.rkt"
+         "../chapter2/2.2.rkt")
+
+(define seg1 (make-segment (make-point 0 0) (make-point 4 0)))
+(define seg2 (make-segment (make-point 0 0) (make-point 0 2)))
+
+; segment length proc tests
+
+(check-equal? (segment-length seg1) 4 "length of seg1 is wrong")
+(check-equal? (segment-length seg2) 2 "length of seg2 is wrong")
+
+; first rectangle implementation method
+
+(check-equal? (square-rect (make-rect-first seg1 seg2) rect-width-first rect-height-first) 8 "square of rectangle with sides 2, 4 is wrong")
+(check-equal? (perim-rect (make-rect-first seg1 seg2) rect-width-first rect-height-first) 12 "perim of rectangle with sides 2, 4 is wrong")
+
+; second rectangle implementation method
+
+(define point-a (make-point 0 2))
+(define point-b (make-point 4 0))
+
+(check-equal? (square-rect (make-rect-second point-a point-b) rect-width-second rect-height-second) 8 "square of rectangle with sides 2, 4 is wrong")
+(check-equal? (perim-rect (make-rect-second point-a point-b) rect-width-second rect-height-second) 12 "perim of rectangle with sides 2, 4 is wrong")
