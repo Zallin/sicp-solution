f358d8031840d683ccf4e0f6556237756dd9adc4
Tim Zallin <tim.zallin@gmail.com>
2015-10-02 22:06:18 +0300

    2.11 solution and tests added

---
+++ test/2.11-test.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+(require rackunit
+         "../chapter2/2.7.rkt"
+         "../chapter2/2.10.rkt"
+         "../chapter2/2.11.rkt")
+
+; intervals multiplication procedure tests
+
+(define a (make-interval 1 2))
+(define b (make-interval -1 2))
+(define c (make-interval -2 -1))
+
+(check-equal? (multiply-intervals a b) (mul-interval a b) "(1; 2) * (-1; 2) failed")
+(check-equal? (multiply-intervals a c) (mul-interval a c) "(1; 2) * (-2; -1) failed")
+(check-equal? (multiply-intervals a a) (mul-interval a a) "(1; 2) * (1; 2) failed")
+
+(check-equal? (multiply-intervals b a) (mul-interval b a) "(-1; 2) * (1; 2) failed")
+(check-equal? (multiply-intervals b c) (mul-interval b c) "(-1; 2) * (-2; -1) failed")
+(check-equal? (multiply-intervals b b) (mul-interval b b) "(-1; 2) * (-1; 2) failed")
+
+(check-equal? (multiply-intervals c a) (mul-interval c a) "(-2; -1) * (1; 2)")
+(check-equal? (multiply-intervals c b) (mul-interval c b) "(-2; -1) * (-1; 2)")
+(check-equal? (multiply-intervals c c) (mul-interval c c) "(-2; -1) * (-2; -1)")
+
+; new interval constructor tests
+
+(check-equal? (make-center-delta 4 50) (make-interval 2 6) "constructing of (2; 6) failed")
+(check-equal? (make-center-delta 2 50) (make-interval 1 3) "constructing of (1; 3) failed")
+(check-equal? (make-center-delta -4 50) (make-interval -6 -2) "constructing of (-6; -2) failed")
