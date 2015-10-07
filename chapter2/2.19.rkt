dc7b727e2ffa0eb1c03a19d18a75b373a171cf15
Tim Zallin <tim.zallin@gmail.com>
2015-10-07 21:12:19 +0300

    2.19 solution and tests added

---
+++ chapter2/2.19.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (no-more? l)
+  (null? l))
+
+(define (except-first-denomination l)
+ (cdr l))
+
+(define (first-denomination l)
+ (car l))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else (+ (cc amount
+                     (except-first-denomination coin-values))
+                 (cc (- amount
+                        (first-denomination coin-values))
+                      coin-values)))))
+
+(provide cc)
