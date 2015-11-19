258f4de273f406c90f0e5054edb3d7c5c8ca49fc
Tim Zallin <tim.zallin@gmail.com>
2015-11-19 17:52:03 +0300

    2.50 solution added

---
+++ chapter2/50/solution.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+; should require all the auxiliary procedures
+
+(define (flip-horiz painter)
+  (transform-painter painter
+		     (make-vect 1.0 0)
+		     (make-vect 0 0)
+		     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+		     (make-vect 1.0 1.0)
+		     (make-vect 0 1.0)
+		     (make-vect 1.0 0)))
+
+(define (rotate270 painter)
+  (transform-painter painter 
+		     (make-vect 0 1.0)
+		     (make-vect 0 0)
+		     (make-vect 1.0 1.0)))
+
+(provide flip-horiz
+	 rotate180
+	 rotate270)
