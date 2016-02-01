c5c27121e6ddda3e96c047973726f249977a2ee3
Tim Zallin <tim.zallin@gmail.com>
2016-02-01 16:58:09 +0300

    3.4 solution and tests added

---
+++ chapter3/4/test.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(require rackunit 
+	 "./4.rkt")
+
+(define acc (make-account 100 'pass))
+
+(check-equal? ((acc 'pass 'withdraw) 40) 60 "witdraw op test failed")
+(check-equal? ((acc 'pass 'deposit) 40) 100 "deposit op test failed")
+(check-equal? ((acc 'word 'withdraw) 100) "wrong password" "password check failed")
+(check-equal? ((acc 'word 'deposit) 100) "wrong password" "password check failed")
+(check-equal? ((acc 'word 'withdraw) 100) "wrong password" "password check failed")
+(check-equal? ((acc 'word 'deposit) 100) "wrong password" "password check failed")
+(check-equal? ((acc 'word 'withdraw) 100) "wrong password" "password check failed")
+(check-equal? ((acc 'word 'deposit) 100) "wrong password" "password check failed")
+(check-equal? ((acc 'word 'withdraw) 100) "wrong password" "password check failed")
+(check-equal? ((acc 'word 'deposit) 100) "cops are on their way" "security system check failed")
+
+
